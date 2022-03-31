e5cb9b4f39d9e192112fc88abbf2168a3d0e4c7d
NYN31 <noyoncse310197@gmail.com>
2022-03-31 18:10:36 +0600

    multi-threaded server convert into executor framework

--- clients/PingClientOne.java
+++ clients/PingClientOne.java
@@ -1,12 +1,5 @@
 package com.socket.clients;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
-
 public class PingClientOne {
     Socket clientSocket;
 
---
+++ useOfExecutorServices/ExecutorPingServerAsync.java
@@ -0,0 +1,42 @@
+package com.socket.useOfExecutorServices;
+
+
+
+public class ExecutorPingServerAsync {
+
+    private final int port;
+    private int clientCount = 1;
+    private ExecutorService executorService;
+
+    public ExecutorPingServerAsync(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server listening at 127.0.0.1: " + port);
+        executorService = Executors.newFixedThreadPool(10);
+        listenAndRespondAsync(serverSocket);
+    }
+
+    public void listenAndRespondAsync(ServerSocket serverSocket) throws IOException{
+        while(!serverSocket.isClosed()){
+            Socket clientSocket = serverSocket.accept();
+            System.out.printf("Client (%s) connected %n", clientCount++);
+            startClientRequestHandlerThread(clientSocket);
+        }
+    }
+
+    public void startClientRequestHandlerThread(Socket clientSocket){
+//        Thread clientHandlerThread = new Thread(new SocketHandler(clientSocket));
+//        clientHandlerThread.setName("Client " + clientCount++);
+//        clientHandlerThread.start();
+
+        executorService.execute(new SocketHandler(clientSocket));
+    }
+
+    public static void main(String[] args) throws IOException {
+        ExecutorPingServerAsync executorPingServerAsync = new ExecutorPingServerAsync(6000);
+        executorPingServerAsync.start();
+    }
+}
